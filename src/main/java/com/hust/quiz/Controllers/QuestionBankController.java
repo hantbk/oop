aa13b5a5fa3ffe015c820a5787b0e1941738632a
TORIOP23 <vinhtran23022003@gmai.com>
2023-05-17 01:58:27 +0700

    combobox category

--- src/main/java/com/hust/quiz/Controllers/QuestionBankController.java
+++ src/main/java/com/hust/quiz/Controllers/QuestionBankController.java
@@ -2,28 +2,67 @@ package com.hust.quiz.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
+import javafx.stage.Popup;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class QuestionBankController implements Initializable {
 
     @FXML
-    private TreeView<String> btn_category;
+    private ComboBox<String> btn_category;
+    @FXML
+    private TreeView<String> treeView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-       createTreeView();
+        createTreeView();
+
+        // configure btn_category
+        btn_category.getParent().setOnMouseClicked(event -> {
+            treeView.setVisible(false);
+        });
+        btn_category.setOnMouseClicked(event -> {
+            treeView.setVisible(!treeView.isVisible());
+        });
+
+        treeView.setOnMouseClicked(event -> {
+            TreeItem<String> selectedItem = treeView.getSelectionModel().getSelectedItem();
+            btn_category.setValue(selectedItem.getValue());
+            if (event.getClickCount() == 2) {
+                treeView.setVisible(!treeView.isVisible());
+            }
+        });
+
+
+
     }
 
     private void createTreeView() {
-        TreeItem<String> rootItem = new TreeItem<>("Root");
-        TreeItem<String> item1 = new TreeItem<>("Item 1");
-        TreeItem<String> item2 = new TreeItem<>("Item 2");
-        rootItem.getChildren().addAll(item1, item2);
+        TreeItem<String> rootNode = new TreeItem<>("Root Node");
+        rootNode.setExpanded(true);
 
-        btn_category.setRoot(rootItem);
+        // Create child nodes
+        TreeItem<String> nodeA = new TreeItem<>("Node A");
+        TreeItem<String> nodeB = new TreeItem<>("Node B");
+        TreeItem<String> nodeC = new TreeItem<>("Node C");
+        TreeItem<String> n1 = new TreeItem<>("Node C");
+        TreeItem<String> n2 = new TreeItem<>("Node C");
+        TreeItem<String> n3 = new TreeItem<>("Node C");
+        TreeItem<String> n4 = new TreeItem<>("Node C");
+        TreeItem<String> n5 = new TreeItem<>("Node C");
+        TreeItem<String> n6 = new TreeItem<>("Node C");
+        TreeItem<String> n7 = new TreeItem<>("Node C");
+        TreeItem<String> n8 = new TreeItem<>("Node C");
+        TreeItem<String> n9 = new TreeItem<>("Node C");
+        TreeItem<String> n10 = new TreeItem<>("Node C");
+        TreeItem<String> n11 = new TreeItem<>("Node C");
+        TreeItem<String> n12 = new TreeItem<>("Node C");
+
+        // Add child nodes to the root node
+        rootNode.getChildren().addAll(nodeA, nodeB, nodeC, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12);
+        treeView.setRoot(rootNode);
     }
 }
