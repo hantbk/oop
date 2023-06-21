b269af55a3c9b27c67e25e06761c01f1545dab47
DoDat_12 <89787228+DoDat-12@users.noreply.github.com>
2023-06-21 23:44:12 +0700

    DoDat fixed UX

--- src/main/java/com/hust/quiz/Controllers/AddQuizController.java
+++ src/main/java/com/hust/quiz/Controllers/AddQuizController.java
@@ -4,16 +4,24 @@ import com.hust.quiz.Views.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddQuizController implements Initializable {
     @FXML
+    private ImageView btn_menu_return;
+    @FXML
     private Button btn_cancel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // configure btn_menu_return - back to home
+        btn_menu_return.setOnMouseClicked(event -> {
+            ViewFactory.getInstance().routes(ViewFactory.SCENES.HOME);
+        });
+        // configure btn_cancel - back to home
         btn_cancel.setOnAction(event -> ViewFactory.getInstance().routes(ViewFactory.SCENES.HOME));
     }
 }
