a86ab6f41738e41a7689559970df495bfc4f212a
Lenhatduc <lenhatduc30042003@gmail.com>
2023-07-15 19:05:17 +0700

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,8 @@ module com.hust.quiz {
     requires javafx.fxml;
     requires java.sql;
     requires mysql.connector.java;
-
-    requires org.apache.poi.ooxml;
-//    requires poi.ooxml;
+    //requires org.apache.poi.ooxml;
+    requires poi.ooxml;
 
     opens com.hust.quiz to javafx.fxml;
     exports com.hust.quiz;
